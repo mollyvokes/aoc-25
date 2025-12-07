95bbe1b2046e404761cd4ba84bfe799983fde147
mollyvokes <mollyrvokes@gmail.com>
2025-12-07 20:27:31 +0000

    correction from strange intellij behaviour

--- src/main/scala/helpers/AdventOfCodeSolver.scala
+++ src/main/scala/helpers/AdventOfCodeSolver.scala
@@ -6,4 +6,12 @@ trait AdventOfCodeSolver {
     import scala.io.Source
     val source = Source.fromFile("src/main/resources/inputs/" + fileName)
     val lines = source.getLines().toList
-    sourc
+    source.close()
+
+    delimiter match {
+      case Some(d) => lines.head.split(d).toList
+      case _ => lines
+    }
+  }
+
+}
