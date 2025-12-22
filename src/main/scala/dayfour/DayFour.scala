dea9828d1f8d97da191ac5b2affb4529ecd4000a
mollyvokes <mollyrvokes@gmail.com>
2025-12-22 17:11:57 +0000

    day 4 part 2

--- src/main/scala/dayfour/DayFour.scala
+++ src/main/scala/dayfour/DayFour.scala
@@ -2,49 +2,61 @@ package dayfour
 
 import helpers.AdventOfCodeSolver
 
+import scala.annotation.tailrec
+
 object DayFour extends AdventOfCodeSolver {
 
   def countSurroundingRolls(sectionToCheck: String): Int = {
     sectionToCheck.count(_.equals('@')) - 1
   }
 
-  def findRolls(diagram: List[String]): List[Int] = {
-    val diagramLength = diagram.length
-    val rowLength = diagram.head.length
+  @tailrec
+  def findRolls(diagramLength: Int, rowLength: Int, diagram: List[String], currCount: Int): Int = {
+    def checkSurroundings(i: Int, j: Int): Int = {
+      val lowerCol = if (j == 0) 0 else j - 1
+      val upperCol = if (j == rowLength - 1) j + 1 else j + 2
+      if (i == 0) {
+        countSurroundingRolls(
+          diagram(i).substring(lowerCol, upperCol) ++
+            diagram(i + 1).substring(lowerCol, upperCol))
+      } else if (i == diagramLength - 1) {
+        countSurroundingRolls(
+          diagram(i - 1).substring(lowerCol, upperCol) ++
+            diagram(i).substring(lowerCol, upperCol))
+      } else {
+        countSurroundingRolls(
+          diagram(i - 1).substring(lowerCol, upperCol) ++
+            diagram(i).substring(lowerCol, upperCol) ++
+            diagram(i + 1).substring(lowerCol, upperCol))
+      }
+    }
 
-    (0 until diagramLength).flatMap(i => {
-      (0 until rowLength).map(j => {
+    var changed = 0
+
+    val newDiagram = (0 until diagramLength).map(i => {
+      (0 until rowLength).foldLeft("")((s, j) => {
         if (diagram(i)(j) == '@') {
-
-          val lowerCol = if (j == 0) 0 else j - 1
-          val upperCol = if (j == rowLength - 1) j + 1 else j + 2
-
-          if (i == 0) {
-            countSurroundingRolls(
-              diagram(i).substring(lowerCol, upperCol) ++
-              diagram(i + 1).substring(lowerCol, upperCol))
-          } else if (i == diagramLength - 1) {
-            countSurroundingRolls(
-              diagram(i - 1).substring(lowerCol, upperCol) ++
-              diagram(i).substring(lowerCol, upperCol))
-          } else {
-            countSurroundingRolls(
-              diagram(i - 1).substring(lowerCol, upperCol) ++
-              diagram(i).substring(lowerCol, upperCol) ++
-              diagram(i + 1).substring(lowerCol, upperCol))
-          }
-        } else 10
+          if (checkSurroundings(i,j) < 4) {
+            changed = changed + 1
+            s + "."
+          } else s + "@"
+        } else s + "."
       })
-    })
-  }.toList
+    }).toList
+
+    if (changed == 0) currCount else findRolls(diagramLength, rowLength, newDiagram, currCount + changed)
+  }
 
   def main(args: Array[String]): Unit = {
 
     val inputLines = readInput("dayfour.txt", None)
 
-    val rolls = findRolls(inputLines)
+    val diagramLength = inputLines.length
+    val rowLength = inputLines.head.length
 
-    print(s"number that can be found ${rolls.count(_ < 4)}")
+    val rollsCount = findRolls(diagramLength, rowLength, inputLines, 0)
+
+    print(s"number that can be found ${rollsCount}")
 
   }
 
